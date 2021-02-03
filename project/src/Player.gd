1f91d0b1d19185699d971b9105a26165b74a3fff
Dan <djgonzalez@bsu.edu>
2021-02-03 00:13:39 -0500

    Added a projectile that the bird shoots down

--- project/src/Player.gd
+++ project/src/Player.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 var screen_size
+var plBomb : = preload("res://project/src/Bomb.tscn")
 
 export var strength : float = 200
 
@@ -8,7 +9,7 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	show()
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("flap"):
 		if Input.is_action_pressed("ui_right"):
 			var direction = Vector2(1, -1).normalized()
@@ -29,3 +30,8 @@ func _process(delta):
 			var direction = Vector2(0, -1).normalized()
 			var velocity = direction * strength
 			apply_impulse(Vector2.ZERO, velocity)
+			
+	if Input.is_action_just_pressed("bomb"):
+		var bomb : = plBomb.instance()
+		bomb.position = position
+		get_tree().current_scene.add_child(bomb)
