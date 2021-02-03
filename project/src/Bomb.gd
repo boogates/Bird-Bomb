9b900877af63169320528f30f38163e9f6e7e057
Dan <djgonzalez@bsu.edu>
2021-02-03 15:43:55 -0500

    Bombs despawn when they leave the screen

--- project/src/Bomb.gd
+++ project/src/Bomb.gd
@@ -5,3 +5,12 @@ export var speed: float = 300
 
 func _physics_process(delta):
 	position.y += speed * delta
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+
+func _on_Bomb_area_entered(area):
+	if area.is_in_group("damageable"):
+		area.damage(1)
--- project/src/Target.gd
+++ project/src/Target.gd
@@ -3,5 +3,12 @@ extends RigidBody2D
 export var min_speed = 25
 export var max_speed = 150
 
+export var life: int = 1
+
+func damage(amount: int):
+	life -= amount
+	if life <= 0:
+		queue_free()
+
 func _ready():
 	pass
